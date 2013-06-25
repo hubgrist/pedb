69acd8105d9c950cfc3c4552183a06b4738188ff
grist <wmacaulay@wilshire.com>
2013-06-26 08:38:40 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pedb::Application.config.secret_token = 'cba978167c9926f05d0703c2c0232e732dfdd8df741786d9cabcecea32be5a24bd3c368f684ee0febf3c98883c0e4e6120f07c0f4e4e771658065bb720d869c6'
