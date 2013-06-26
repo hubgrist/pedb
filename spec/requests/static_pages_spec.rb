17e6524ebb1ae55cf36e7b483bf0f18dfb2cef37
grist <wmacaulay@wilshire.com>
2013-06-26 16:20:58 +1000

    Finish layout and routs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,57 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	let(:base_title) {"Private Equity Database"}
+
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h2', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
 
   describe "Home page" do
+    before { visit root_path}
+    let(:heading) {'Private Equity Database'}
+    let(:page_title) {''}
 
-    it "should have the content 'Private Equity Database'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Private Equity Database')
-    end
-
-    it "should have the right title" do
-    	visit '/static_pages/home'
-    	page.should have_selector('title', :text => "Private Equity Database | Home")
-    end
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
-  		page.should have_selector('h1', :text => 'Help')
-  	end
-
-  	it "should have the right title" do
-  		visit '/static_pages/help'
-  		page.should have_selector('title', :text => "Private Equity Database | Help")
-  	end
+    before { visit help_path }
+    let(:heading) {'Help'}
+    let(:page_title) { 'Help'}
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
-  	it "should have the content 'About'" do
-  		visit '/static_pages/about'
-  		page.should have_selector('h1', :text => 'About')
-  	end
+  	before { visit about_path }
+    let(:heading) {'About'}
+    let(:page_title) { 'About'}
+    it_should_behave_like "all static pages"
+  end
 
-  	it "should have the right title" do
-  		visit '/static_pages/about' 
-  		page.should have_selector('title', :text => "Private Equity Database | About")
-  	end
+  describe "Contact page" do
+    before { visit contact_path }
+  	let(:heading) {'Contact'}
+    let(:page_title) { 'Contact Us'}
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact Us')
+    click_link "Home"
+    click_link "sign up"
+    page.should have_selector 'title', text: full_title('sign up')
+    click_link "PEDB"
+    page.should have_selector 'title', text: full_title('')
   end
 end
